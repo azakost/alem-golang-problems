c1db15f9d8261cfd9b7b9417d5fca7317ea59486
azakost <azakost@gmail.com>
2020-03-06 15:28:42 +0600

    commited

--- listsizeprog/main.go
+++ listsizeprog/main.go
@@ -1,11 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"os/exec"
-)
+import "fmt"
 
 func main() {
-	out, _ := exec.Command("curl", "https://google.com").Output()
-	fmt.Print(string(out))
+	var list List
+	pushBack(&list, &NodeL{Data: 1})
+	pushBack(&list, &NodeL{Data: 1})
+	fmt.Print(ListSize(&list))
+}
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func pushBack(l *List, d *NodeL) {
+	if l.Head == nil {
+		l.Head = d
+		return
+	}
+	node := l.Head
+	for node.Next != nil {
+		node = node.Next
+	}
+	node.Next = d
+}
+
+func ListSize(l *List) int {
+	if l.Head == nil {
+		return 0
+	}
+
+	node := l.Head
+	n := 1
+	for node.Next != nil {
+		node = node.Next
+		n++
+	}
+	return n
 }
