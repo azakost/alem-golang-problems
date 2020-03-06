bb85b0930ff6fb12a88d19856d642d86d9f3112e
azakost <azakost@gmail.com>
2020-03-06 22:49:30 +0600

    commited

---
+++ balancedstring/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	ar := os.Args
+
+	if len(ar) == 2 {
+		fmt.Print(balanced(ar[1]))
+	}
+	fmt.Println()
+}
+
+func balanced(s string) int {
+	n := 0
+	k := 0
+	for i, r := range []rune(s) {
+		if r == 'C' {
+			n++
+		}
+		if r == 'D' {
+			n--
+		}
+		if n == 0 && i != 0 {
+			k++
+		}
+	}
+	return k
+}
